e83f61a2c2f1eddb584b4f39b1990b14a9a002e9
Deborah Miranda Casanova Costa <dehmirandac2@Deborahs-MacBook-Pro.local>
2019-06-02 21:40:09 -0300

    feat: adicionando o useEffect do react hooks para mudar o título da página

--- src/components/contador.js
+++ src/components/contador.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Contador = () => {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
+
+  // Similar ao componentDidMount e componentDidUpdate:
+  useEffect(() => {
+    // Atualiza o titulo do documento usando a API do browser
+    document.title = `Você clicou ${count} vezes`;
+  }, [count]); // Apenas re-execute o efeito quando o count mudar;
 
   return (
     <div>
